3081c2f17378cc738c640a273c0df7266a1d0b79
Piorunx22 <piorunx90@gmail.com>
2024-02-22 15:55:39 +0100

    :construction: Initial support for multiple teams

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,11 +3,29 @@ import TeamRow from "@/components/TeamBuilder/TeamRow";
 import { useState } from "react";
 
 export default function Main() {
-  const [team, setTeam] = useState();
+  const [teams, setTeams] = useState([{ teamName: "team", team: [] }]);
+
+  const addTeam = () => {
+    const arr = [...teams];
+    arr.push({ teamName: "team", team: [] });
+    setTeams(arr);
+  };
+
+  const updateTeam = (index: any, updatedTeam: any) => {
+    setTeams((prevTeams) => prevTeams.map((team, i) => (i === index ? updatedTeam : team)));
+  };
   return (
     <main>
-      <TeamRow setTeam={setTeam} />
-      <pre>{JSON.stringify(team, null, 2)}</pre>
+      {teams.map((team, index) => (
+        <TeamRow
+          key={index}
+          teamIndex={index}
+          team={team}
+          setTeam={(updatedTeam: any) => updateTeam(index, updatedTeam)}
+        />
+      ))}
+      <button onClick={addTeam}>Add team</button>
+      <pre>{JSON.stringify(teams, null, 2)}</pre>
     </main>
   );
 }
--- src/components/TeamBuilder/TeamRow.tsx
+++ src/components/TeamBuilder/TeamRow.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import CharacterSlot from "./CharacterSlot";
 
-function TeamRow({ setTeam }) {
+function TeamRow({ team, setTeam, teamIndex }) {
   const [slot1, setSlot1] = useState();
   const [slot2, setSlot2] = useState();
   const [slot3, setSlot3] = useState();
@@ -12,29 +12,32 @@ function TeamRow({ setTeam }) {
   const saveTeam = () => {
     const obj = [slot1, slot2, slot3, slot4];
 
-    setTeam(obj);
+    setTeam({ teamName: team.teamName, team: obj });
   };
 
   useEffect(() => saveTeam(), [slot1, slot2, slot3, slot4]);
 
   return (
-    <div className="grid grid-cols-4 justify-items-center">
-      <CharacterSlot
-        data={slot1}
-        setData={setSlot1}
-      />
-      <CharacterSlot
-        data={slot2}
-        setData={setSlot2}
-      />
-      <CharacterSlot
-        data={slot3}
-        setData={setSlot3}
-      />
-      <CharacterSlot
-        data={slot4}
-        setData={setSlot4}
-      />
+    <div>
+      <p>{team.teamName}</p>
+      <div className="grid grid-cols-4 justify-items-center">
+        <CharacterSlot
+          data={slot1}
+          setData={setSlot1}
+        />
+        <CharacterSlot
+          data={slot2}
+          setData={setSlot2}
+        />
+        <CharacterSlot
+          data={slot3}
+          setData={setSlot3}
+        />
+        <CharacterSlot
+          data={slot4}
+          setData={setSlot4}
+        />
+      </div>
     </div>
   );
 }
