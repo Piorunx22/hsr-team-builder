edfe17ebc4985a3e73cfcc29e6046675eabe9a78
Piorunx22 <piorunx90@gmail.com>
2024-02-22 15:01:42 +0100

    :sparkles: Make `level` optional, display rarity gradient

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+## 0.1.1
+
+- `ItemIcon` now displays rarity
+- Made `level` prop in `ItemIcon` optional
+
+# 0.1.0
+
+- Game data is now being imported
+
+## 0.0.3
+
+- Improved localization logic:
+  - Added dictionary context
+  - Added support for variables
+  - Removed `server-only` clause from `getDictionary.ts`
+
+## 0.0.2
+
+- Added levels to Characters and Light Cones
+
+## 0.0.1
+
+- working team builder prototype
+- basic localization is working
--- src/components/TeamBuilder/ItemIcon.tsx
+++ src/components/TeamBuilder/ItemIcon.tsx
@@ -1,32 +1,45 @@
 "use client";
 import { useDictionary } from "@/contexts/DictionaryContext";
+import { useGameData } from "@/contexts/GameDataContext";
 import Image from "next/image";
 
 interface ItemIconProps {
   type: "character" | "light_cone";
   id: string | null;
-  level: number;
+  level?: number;
 }
 
 function ItemIcon({ type, id, level }: ItemIconProps) {
-  if (!id || (type == "character" && id.length != 4) || (type == "light_cone" && id.length != 5)) {
+  const t = useDictionary();
+  const gameData = useGameData();
+  let item;
+
+  if (id && ((type == "character" && id.length == 4) || (type == "light_cone" && id.length == 5))) {
+    item = gameData[`${type}s`][id];
+  }
+
+  if (!id || !item) {
     return (
       <div className="w-[100px] h-[100px] flex justify-center items-center bg-neutral-300">?</div>
     );
   }
-  const t = useDictionary();
+
+  const rarity = item.rarity;
+
   return (
-    <div className="w-[100px] h-[100px] relative">
+    <div className={`w-[100px] h-[100px] relative rarity-${rarity}`}>
       <Image
         className="absolute"
         width={100}
         height={100}
-        src={process.env.NEXT_PUBLIC_BASE_DATA_URL + `/icon/${type}/${id}.png`}
+        src={process.env.NEXT_PUBLIC_BASE_DATA_URL + "/" + item.icon}
         alt={id}
       />
-      <span className="absolute top-0 left-0 backdrop-blur-sm">
-        {t("common.level.short", { LEVEL: level })}
-      </span>
+      {level ? (
+        <span className="absolute top-0 left-0 backdrop-blur-sm">
+          {t("common.level.short", { LEVEL: String(level) })}
+        </span>
+      ) : null}
     </div>
   );
 }
