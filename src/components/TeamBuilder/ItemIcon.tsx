f0a1a0c0a6e37c6a8fa04a947d613c63cff5f883
Piorunx22 <piorunx90@gmail.com>
2024-02-23 16:41:03 +0100

    :sparkles: `size` prop for `ItemIcon`

--- src/components/TeamBuilder/ItemIcon.tsx
+++ src/components/TeamBuilder/ItemIcon.tsx
@@ -5,14 +5,28 @@ import Image from "next/image";
 
 interface ItemIconProps {
   type: "character" | "light_cone";
-  id: string | null;
+  id: string | null | undefined;
   level?: number;
+  size?: number;
 }
 
-function ItemIcon({ type, id, level }: ItemIconProps) {
+function ItemIcon({ type, id, level, size = 100 }: ItemIconProps) {
   const t = useDictionary();
   const gameData = useGameData();
   let item;
+
+  // if (!height) {
+  //   if (width) height = width;
+  //   else height = 100;
+  // }
+
+  // if (!width) {
+  //   if (height) width = height;
+  //   else width = 100;
+  // }
+
+  // height ?? (width ? (height = width) : (height = 100));
+  // width ?? (height ? (width = height) : (width = 100));
 
   if (id && ((type == "character" && id.length == 4) || (type == "light_cone" && id.length == 5))) {
     item = gameData[`${type}s`][id];
@@ -20,18 +34,26 @@ function ItemIcon({ type, id, level }: ItemIconProps) {
 
   if (!id || !item) {
     return (
-      <div className="w-[100px] h-[100px] flex justify-center items-center bg-neutral-300">?</div>
+      <div
+        style={{ width: size, height: size }}
+        className={`flex justify-center items-center bg-neutral-300`}
+      >
+        ?
+      </div>
     );
   }
 
   const rarity = item.rarity;
 
   return (
-    <div className={`w-[100px] h-[100px] relative rarity-${rarity}`}>
+    <div
+      style={{ width: size, height: size }}
+      className={`relative rarity-${rarity}`}
+    >
       <Image
         className="absolute"
-        width={100}
-        height={100}
+        width={size}
+        height={size}
         src={process.env.NEXT_PUBLIC_BASE_DATA_URL + "/" + item.icon}
         alt={id}
       />
